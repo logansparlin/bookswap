08da176760d85c4427ba1c0de5307a26a6d4382f
Logan Sparlin <logan.sparlin@gmail.com>
2015-04-02 10:23:11 -0500

    reset input value when form is closed

--- client/views/accounts/accounts.js
+++ client/views/accounts/accounts.js
@@ -2,6 +2,7 @@ Template.accounts.events({
 	'click .close-icon': function() {
 		$('body').removeClass('login--active')
 		$('body').removeClass('register--active')
+		$('.accounts-form input').val('');
 		Router.go('/')
 	}
 })
