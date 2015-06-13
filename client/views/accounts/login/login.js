d3ca8d763d195e5a4b84ade73953033b4cd331ce
logansparlin <logan.sparlin@gmail.com>
2015-06-12 22:36:17 -0500

    no message

--- client/views/accounts/login/login.js
+++ client/views/accounts/login/login.js
@@ -13,7 +13,6 @@ Template.login.helpers({
 Template.login.events({
 	'submit form': function(e) {
 		e.preventDefault()
-		console.log('Input Form Submitted')
 		var email = $('#login-email').val();
 		var password = $('#login-password').val();
 		Meteor.loginWithPassword(email, password, function(err) {
